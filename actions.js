9dd5003b10c65e94d9ec11ca17051973d40ad855
Jorge Ferreiro <jgferreiro.me@gmail.com>
2015-03-04 22:24:01 +0100

    JS: Adding text dissappeared and margin

--- actions.js
+++ actions.js
@@ -2,7 +2,8 @@ var header = $('.header'),
 	homeSection = $('.home'),
 	text = $('.homeHeadline'),
 	subtext = $('.homeSubheadline'),
-	scroll = window.scrollY;
+	margin = 1;
+
  
 	$(window).scroll( function() {
 		var scroll = window.scrollY;
@@ -11,11 +12,24 @@ var header = $('.header'),
 		// console.log(opacity);
 
 		if ((scroll >= 0) && (scroll <= homeSection.height() - 80)) {
+			margin += 3;
 			header.removeClass('headerFixed');
-			text.css("opacity", opacity);
-			subtext.css("opacity", opacity + 0.5);
+			text.css({
+				"opacity": opacity, 
+				"margin-top": margin
+			});
+			subtext.css({
+				"opacity": opacity + 0.5, 
+				"margin-top": margin
+			}); 
 		} else {
 			header.addClass('headerFixed'); 
+			text.css({
+				"margin-top": margin - 3
+			});
+			subtext.css({
+				"margin-top": margin - 3
+			}); 
 		}
 	});  
 
